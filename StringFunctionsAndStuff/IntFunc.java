387f58e5a02373f3b80d9c023b1742328ab29934
WalidAbdul132 <abdulbuster132@gmail.com>
2023-04-01 15:02:03 +0400

    Create IntFunc.java

---
+++ StringFunctionsAndStuff/IntFunc.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package StringFunctionsAndStuff;
+
+/**
+ *
+ * @author Hp
+ */
+public class IntFunc {
+    public static void main(String []args){
+       int num = 3345;
+       System.out.println(num);
+       System.out.println(Math.incrementExact(num));
+       System.out.println(Math.multiplyExact(num,2));
+       System.out.println(Math.addExact(num, 10));
+       System.out.println(Math.divideExact(num,5));
+       System.out.println(Math.sqrt(num));
+       
+    } 
+}
